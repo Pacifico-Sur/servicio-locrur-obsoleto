9bf49d9133a70bd0e1587db60103a571cc856475
davidmacer <jdavid.macer@gmail.com>
2023-07-05 17:50:32 -0600

    Grafica datos de localidades con geometría tipo polígono

--- app/www/scripts/grafica_municipio_localidades.R
+++ app/www/scripts/grafica_municipio_localidades.R
@@ -1,35 +1,59 @@
 #!/usr/bin/env Rscript
-library(DBI)
-library(dplyr)
+# Este script crea el mapa que contiene el contorno del polígono del municipio
+# elegido por el usuario y las localidades rurales dentro del municipio. Las 
+# localidades están representadas (según el censo del inegi) por polígonos 
+# (aquellas localidades que están amanzanadas y por puntos (aquellas localidades 
+# que no están amanzanadas).
+
+# Se conecta con la base de datos de localidades rurales
+# El host, puerto, user y password están definidas en el docker-compose.yml del 
+# repositorio database (https://github.com/Pacifico-Sur/database)
+library(ipa)
 library(ggplot2)
 library(magrittr)
-library(sf)
-library(ipa)
 
 con <- DBI::dbConnect(RPostgres::Postgres(), dbname = "siclr_db",
-                 host = "172.16.238.10", port = "5432",
-                 user = "postgres", password = "postgres")
+                      host = "database", port = "5432",
+                      user = "postgres", password = "postgres")
 
+# Toma como argumento el ID del municipio seleccionado por el usuario
 args <- commandArgs(TRUE)
-id_muni <- args[1]
+# id_muni <- args[1]
+id_muni <- 1458
 
-query_mun <- paste("SELECT * from edo_mun.municipios WHERE ", "\"ID_MUN\"", " = ", id_muni, ";")
+# Extrae los datos de municipio, estado, localidades (puntos) y localidades 
+# (polígonos)
+query_mun <- paste("SELECT * from edo_mun.municipios WHERE ",
+                   "\"ID_MUN\"", " = ", id_muni, ";")
 municipio <- ipa::db_get_table(con, query_mun)
 
-query_edo <- paste("SELECT * from edo_mun.estados WHERE ", "\"ID_ENT\"", " = ", municipio$id_ent, ";")
+query_edo <- paste("SELECT * from edo_mun.estados WHERE ",
+                   "\"ID_ENT\"", " = ", municipio$id_ent, ";")
 estado <- ipa::db_get_table(con, query_edo)
 
-query_loc <- "SELECT * FROM loc.localidades"
-loc <- ipa::db_get_table(con, query_loc)
+query_loc_puntos <- paste("SELECT * FROM loc.localidades ",
+                          "WHERE \"ID_MUN\"", "= ", id_muni, " AND",
+                          "\"POBTOT\" >= 100 AND \"POBTOT\" <= 2500;")
+loc_puntos <- ipa::db_get_table(con, query_loc_puntos)
 
-localidad <- loc %>% 
-  filter(id_mun == municipio$id_mun, pobtot >= 100 & pobtot <= 2500)
+query_loc_poligonos <- "SELECT * FROM loc.loc_poli WHERE \"TIPO\" = \'Rural\';"
+loc_poli <- ipa::db_get_table(con, query_loc_poligonos)
+
+loc_poli <- loc_poli %>%
+  filter(cve_ent == estado$cve_ent,
+         cve_mun == municipio$cve_mun,
+         cve_loc %in% loc_puntos$cve_loc)
+loc_poli <- loc_poli %>%
+  st_set_crs(st_crs(loc_puntos))
+
+loc_puntos <- loc_puntos %>%
+  filter(!cve_loc %in% loc_poli$cve_loc)
 
 # Define un mapa base por defecto
 basemaps::set_defaults(
   map_service = "osm", 
   map_type = "streets",
-  map_res = 0.5)
+  map_res = 0.75)
 
 # Llama la función png para inicializar el gráfico
 save_path <- "../temp-img/"
@@ -42,13 +66,15 @@ png(file = paste0(save_path, "mi_mapa.png"),
     res = 100)
 
 # Crea la gráfica con el diseño necesario
-municipio <- municipio %>% st_transform(crs = st_crs(3857))
-
 ggplot() +
-  basemaps::basemap_gglayer(municipio %>% st_transform(crs = st_crs(3857))) + 
-  geom_sf(data = municipio %>% st_transform(crs = st_crs(3857)), 
+  basemaps::basemap_gglayer(municipio) +
+  geom_sf(data = municipio %>% st_transform(crs = st_crs(3857)),
           fill = "blue", alpha = 0.030, colour = "black") +
-  geom_sf(data = localidad %>% st_transform(crs = st_crs(3857))) +
+  geom_sf(data = loc_puntos) +
+  geom_sf(data = loc_poli,
+          color = "black",
+          fill = "black",
+          alpha = 0.6) +
   coord_sf(expand = FALSE) +
   scale_fill_identity() +
   labs(title = paste0("Municipio de ", municipio$nomgeo, ", ", estado$nomgeo),
